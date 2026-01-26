4c8677750972c30ee4835c16bde37e6d14bc7686
Veronika050987 <veronicaviv@mail.ru>
2026-01-26 19:32:56 +0300

    player done and date/time corrections in progress

--- JavaScript/events/functions.js
+++ JavaScript/events/functions.js
@@ -16,25 +16,24 @@ function setBackground() {
     document.getElementById('color-sample').style.width = "200px";
     document.getElementById('color-sample').style.height = "200px";
 }
-function switchBackground()
-{
+function switchBackground() {
     //let target = document.getElementById("switch-background").src;
     //let path = target.split('/');
     //let file = path[path.length - 1];
-    /////////////////////////////////////
+    //////////////////////////////////////////////////
     //console.log(file);
     //if (file === "moon.png") document.getElementById("switch-background").src = "img/sun.png";
     //else document.getElementById("switch-background").src = "img/moon.png";
     //document.getElementById("switch-background").src = file === "moon.png" ? "img/sun.png" : "img/moon.png";
     //document.body.style.backgroundImage = "none";
     let delay = document.getElementById("delay").value;
-    document.body.style.transition = `background-image ${delay}s, color ${delay}s`;
+    document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
     document.getElementById("switch-background").style.transition = `background-image ${delay}s, filter ${delay}s`;
     document.body.className = document.body.className === "dark" ? "white" : "dark";
-    /*document.body.className = file === "moon.png" ? "dark" : "white";*/
+    //document.body.className = file === "moon.png" ? "dark" : "white";
     //document.body.style.backgroundColor = file === "moon.png" ? "black" : "white";
     //document.body.style.color = file === "moon.png" ? "white" : "black";
-    /*document.getElementById("switch-background").src = `img/${file === "moon.png" ? "sun.png" : "moon.png"}`;*/
+    //document.getElementById("switch-background").src = `img/${file === "moon.png" ? "sun.png" : "moon.png"}`;
     /*
     ---------------------
     5 == "5":true;
@@ -42,25 +41,22 @@ function switchBackground()
     ---------------------
     */
 }
-
 document.addEventListener
-(
+    (
         "mousemove",
-        function (event)
-        {
+        function (event) {
             let x = event.clientX;
             let y = event.clientY;
             document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y}`;
         }
-);
+    );
 ///////////////////////////////////////////////////////////////////////////////////
 //DOM - Document Object Model
-function addLeadingZero(number)
-{
+function addLeadingZero(number) {
+    //numer = Number(number);
     return number < 10 ? "0" + `${number}` : `${number}`;
 }
-document.body.onload = function tick_timer()
-{
+document.body.onload = function tick_timer() {
     let time = new Date();
     document.getElementById("full-time").innerHTML = time;
     document.getElementById("hours").innerHTML = addLeadingZero(time.getHours());
@@ -78,49 +74,46 @@ document.body.onload = function tick_timer()
 
     setTimeout(tick_timer, 100);
 }
-
-document.getElementById("btn-start").onclick = function startCountdownTimer()
-{
+document.getElementById("btn-start").onclick = function startCountdownTimer() {
     let targetDate = document.getElementById("target-date");
     let targetTime = document.getElementById("target-time");
     let btnStart = document.getElementById("btn-start");
-    if (btnStart.value === "Start")
-    {
+    if (btnStart.value === "Start") {
         btnStart.value = "Stop";
         targetDate.disabled = targetTime.disabled = true;
         tickCountdown();
     }
-    else
-    {
+    else {
         btnStart.value = "Start";
         targetDate.disabled = targetTime.disabled = false;
         clearTimeout(tickCountdown);
+        resetDisplay();//Это должно быть в if()-е
     }
 
-    let display = document.getElementById("display");
-    //////////////////////////////////////
-    let append = document.createElement("div");
-    append.id = "appended";
-    append.innerHTML = "Appended element";
-    display.append(append);
-    //////////////////////////////////////
-    let prepend = document.createElement("div");
-    prepend.id = "prepended";
-    prepend.innerHTML = "Prepended element";
-    display.prepend(prepend);
-    //////////////////////////////////////
-    let before = document.createElement("h3");
-    before.id = "before-display";
-    before.innerHTML = "Time left since the beginning";
-    display.before(before);
-    //////////////////////////////////////
-    let after = document.createElement("h4");
-    after.id = "paradise";
-    after.innerHTML = "Сюда нужно будет прикрутить музон";
-    display.after(after);
+    //let display = document.getElementById("display");
+    ////////////////////////////////////////
+    //let append = document.createElement("div");
+    //append.id = "appended";
+    //append.innerHTML = "Appended element";
+    //display.append(append);
+    ////////////////////////////////////////
+    //let prepend = document.createElement("div");
+    //prepend.id = "prepended";
+    //prepend.innerHTML = "Prepended element";
+    //display.prepend(prepend);
+    ////////////////////////////////////////
+    //let before = document.createElement("h3");
+    //before.id = "before-display";
+    //before.innerHTML = "Time left since the beginning";
+    //display.before(before);
+    ////////////////////////////////////////
+    //let after = document.createElement("h4");
+    //after.id = "paradise";
+    //after.innerHTML = "Сюда нужно будет прикрутить музон";
+    //display.after(after);
 }
-function tickCountdown()
-{
+function tickCountdown() {
+    if (document.getElementById("btn-start").value === "Start") return;
     let now = new Date();
 
     let targetDateControl = document.getElementById("target-date");
@@ -147,6 +140,8 @@ function tickCountdown()
     document.getElementById("duration").innerHTML = duration;
 
     let timestamp = Math.trunc(duration / 1000);
+    document.getElementById("signature").innerHTML = timestamp > 0 ? "Времени осталось" : "Времени прошло";
+    if (timestamp < 0) timestamp = -timestamp;
     document.getElementById("timestamp").innerHTML = timestamp;
 
     const SECONDS_PER_MINUTE = 60;
@@ -162,32 +157,62 @@ function tickCountdown()
     date = date * SECONDS_PER_DAY;
 
     let years = Math.floor(date / SECONDS_PER_YEAR);
-    if (years > 0)
-    {
+    if (years > 0) {
         date = date % SECONDS_PER_YEAR;
         let years_unit = document.getElementById("years-unit");
-        if (years_unit == null)
-        {
-            let display = document.getElementById("display");
-            display.prepend(createTimeBlock("years", addLeadingZero(years)));
+        if (years_unit == null) {
+            //let display = document.getElementById("display");
+            //display.prepend(createTimeBlock("years", addLeadingZero(years)));
+
+            //      ???
+            let years_block = createTimeBlock("years", years);
+            let hours_block = document.getElementById("hours-unit").parentElement;
+            hours_block.before(years_block);
+            //      ???
         }
         else years_unit.innerHTML = addLeadingZero(years);
     }
     else removeTimeBlock("years");
+
     let months = Math.floor(date / SECONDS_PER_MONTH);
-    if (months > 0)
-    {
-        let display = document.getElementById("display");
+    if (months > 0) {
+        //let display = document.getElementById("display");
         date = date % SECONDS_PER_MONTH;
         let months_unit = document.getElementById("months-unit");
-        if (months_unit == null)
-        {
+        if (months_unit == null) {
             months_block = createTimeBlock("months", addLeadingZero(months));
             let hours_block = document.getElementById("hours-unit").parentElement;
             hours_block.before(months_block);
         }
         else months_unit.innerHTML = addLeadingZero(months);
     }
+    else removeTimeBlock("months");
+
+    let weeks = Math.floor(date / SECONDS_PER_WEEK);
+    if (weeks > 0) {
+        date = date % SECONDS_PER_WEEK;
+        let weeks_unit = document.getElementById("weeks-unit");
+        if (weeks_unit == null) {
+            weeks_block = createTimeBlock("weeks", addLeadingZero(weeks));
+            let hours_block = document.getElementById("hours-unit").parentElement;
+            hours_block.before(weeks_block);
+        }
+        else weeks_unit.innerHTML = addLeadingZero(weeks);
+    }
+    else removeTimeBlock("weeks");
+
+    let days = Math.floor(date / SECONDS_PER_DAY);
+    if (days > 0) {
+        let days_unit = document.getElementById("days-unit");
+        if (days_unit == null) {
+            days_block = createTimeBlock("days", addLeadingZero(days));
+            let hours_block = document.getElementById("hours-unit").parentElement;
+            hours_block.before(days_block);
+        }
+        else days_unit.innerHTML = addLeadingZero(days);
+    }
+    else removeTimeBlock("days");
+    ////////////////////////////////////////////////////////////////////////
 
     let hours = Math.floor(time_of_day / SECONDS_PER_HOUR);
     if (hours > 0) time_of_day = (time_of_day % (SECONDS_PER_HOUR));
@@ -201,12 +226,16 @@ function tickCountdown()
     document.getElementById("minutes-unit").innerHTML = addLeadingZero(minutes);
     document.getElementById("seconds-unit").innerHTML = addLeadingZero(seconds);
 
-    if (timestamp > 0 && document.getElementById("btn-start").value === "Stop")
+    //if (timestamp > 0 && document.getElementById("btn-start").value === "Stop")
+    if (document.getElementById("btn-start").value === "Stop")
         setTimeout(tickCountdown, 100);
+    if (timestamp == 0) {
+        let player = document.getElementById("player");
+        player.play();
+    }
 }
 
-function createTimeBlock(name, value)
-{
+function createTimeBlock(name, value) {
     let time_block = document.createElement("div");
     time_block.className = "time-block";
 
@@ -227,13 +256,22 @@ function createTimeBlock(name, value)
 
     return time_block;
 }
-function removeTimeBlock(name)
-{
+function removeTimeBlock(name) {
     let unit = document.getElementById(`${name}-unit`);
-    if (unit != null)
-    {
+    if (unit != null) {
         let block = unit.parentElement;
         let display = block.parentElement;
         display.removeChild(block);
     }
+}
+function resetDisplay() {
+    let display = document.getElementById("display");
+    //display.innerHTML = "";
+    console.log(display.children.length);
+    let children = display.children;
+    console.log(children);
+    console.log(display.children[0]);
+    while (display.children[0].children[0].id != "hours-unit")
+        display.children[0].remove();
+    //removeTimeBlock("days");
 }
