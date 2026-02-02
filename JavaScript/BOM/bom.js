cb7c021b6662434319386b4cf49d2682c7ce49cb
Veronika050987 <veronicaviv@mail.ru>
2026-02-02 20:04:12 +0300

    bom

--- JavaScript/BOM/bom.js
+++ JavaScript/BOM/bom.js
@@ -1,10 +1,12 @@
 ﻿// JavaScript source code
-function GetWindowSize() {
+function GetWindowSize()
+{
     let window_width = window.innerWidth;
     let window_height = window.innerHeight;
     document.getElementById("window-size").innerHTML = `Размер окна браузера: ${window_width}x${window_height}`;
 }
-function GetScreenProperties() {
+function GetScreenProperties()
+{
     let screen_width = window.screen.width;
     let screen_height = window.screen.height;
     document.getElementById("screen-resolution").innerHTML = `${screen_width}x${screen_height}`;
@@ -12,14 +14,16 @@ function GetScreenProperties() {
     document.getElementById("color-depth").innerHTML = `${window.screen.colorDepth} bit;`;
     document.getElementById("pixel-depth").innerHTML = `${window.screen.pixelDepth} bit`;
 }
-function GetLocation() {
+function GetLocation()
+{
     document.getElementById("bom-href").innerHTML = window.location.href;
     document.getElementById("bom-hostname").innerHTML = window.location.hostname;
     document.getElementById("bom-pathname").innerHTML = window.location.pathname;
     document.getElementById("bom-protocol").innerHTML = window.location.protocol;
     document.getElementById("bom-port").innerHTML = window.location.port;
 }
-function GoTo() {
+function GoTo()
+{
     let bom_url = document.getElementById("bom-url").value;
     window.location.assign(bom_url);
 }
